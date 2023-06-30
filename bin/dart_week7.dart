6c1e5e5cb8640ff9fba0f3ea78c3911fe76e6138
chansak04 <deawchansak@gmail.com>
2023-06-30 09:34:31 +0700

    first commit

---
+++ bin/dart_week7.dart
@@ -0,0 +1,12 @@
+void main(){
+  print("Logic Operator");
+   int x = 10;
+   int y = 10;
+   bool a = true;
+   bool b = false;
+
+
+   print("x==y = ${x==y}");//equal
+   print("a|b = ${a|b}");//OR gate
+   print("a&b = ${a|b}");//and gate
+}
